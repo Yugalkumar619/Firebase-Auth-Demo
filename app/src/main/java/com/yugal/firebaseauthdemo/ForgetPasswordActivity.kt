b43e4f31c684f7fe5c4b858e9b18451a0e2c4bae
Yugalkumar619 <71225190+Yugalkumar619@users.noreply.github.com>
2021-06-28 14:07:27 +0530

    Reset Password

--- app/src/main/java/com/yugal/firebaseauthdemo/ForgetPasswordActivity.kt
+++ app/src/main/java/com/yugal/firebaseauthdemo/ForgetPasswordActivity.kt
@@ -2,10 +2,42 @@ package com.yugal.firebaseauthdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_forget_password.*
 
 class ForgetPasswordActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_forget_password)
+
+        btn_forgetPassword.setOnClickListener{
+            val email: String = et_email_fp.text.toString().trim() { it <= ' '}
+            if(email.isEmpty()){
+                Toast.makeText(
+                    this@ForgetPasswordActivity,
+                    "Please enter email address.",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }else{
+                FirebaseAuth.getInstance().sendPasswordResetEmail(email)
+                    .addOnCompleteListener{task ->
+                        if(task.isSuccessful){
+                            Toast.makeText(
+                                this@ForgetPasswordActivity,
+                                "Email sent successfully to reset your password!",
+                                Toast.LENGTH_LONG
+                            ).show()
+                            finish()
+                        }else{
+                            Toast.makeText(
+                                this@ForgetPasswordActivity,
+                                task.exception!!.message.toString(),
+                                Toast.LENGTH_LONG
+                            ).show()
+                        }
+                    }
+            }
+        }
     }
 }
