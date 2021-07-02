125c0f56f4c7aa42c9e58865d03ef06c09548230
Yugalkumar619 <71225190+Yugalkumar619@users.noreply.github.com>
2021-07-02 11:49:54 +0530

    Give permission to set photo

--- app/src/main/java/com/yugal/firebaseauthdemo/MainPageActivity.kt
+++ app/src/main/java/com/yugal/firebaseauthdemo/MainPageActivity.kt
@@ -1,31 +1,79 @@
 package com.yugal.firebaseauthdemo
 
+import android.Manifest
 import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main_page.*
+import java.io.IOException
 
 class MainPageActivity : AppCompatActivity() {
+
+    private var mSelectedImageFileUri: Uri? = null
+
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_page)
 
-        val userId = intent.getStringExtra("user_id")
-        val emailId = intent.getStringExtra("email_id")
+        btn_select_image.setOnClickListener{
+            if(ContextCompat.checkSelfPermission(this,Manifest.permission.READ_EXTERNAL_STORAGE
+                ) == PackageManager.PERMISSION_GRANTED){
+                // Select image
+                val galleryIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                startActivityForResult(galleryIntent, 222)
+            }else{
+                // Request permission
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                    121)
+            }
+        }
 
-        tv_email_id.text = "User ID :: $userId"
-        tv_user_id.text = "Email ID :: $emailId"
+    }
 
-        btn_logout.setOnClickListener(){
-
-            // Logout from app.
-            FirebaseAuth.getInstance().signOut()
-
-            startActivity(Intent(this@MainPageActivity, MainActivity::class.java))
-            finish()
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode == 121){
+            val galleryIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            startActivityForResult(galleryIntent, 222)
+        }else{
+            Toast.makeText(
+                this,
+                "Oops, you just denied the permission for storage, You can also allow it from settings.",
+                Toast.LENGTH_LONG
+            ).show()
         }
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode == Activity.RESULT_OK){
+            if(requestCode == 222){
+                if(data != null){
+                    try {
+                        mSelectedImageFileUri = data?.data
+                        image_view.setImageURI(mSelectedImageFileUri)
+                    }catch (e: IOException){
+                        e.printStackTrace()
+                        Toast.makeText(this@MainPageActivity, "Image selection Failed!",
+                            Toast.LENGTH_LONG).show()
+                    }
+                }
+            }
+        }
+    }
+
 }
